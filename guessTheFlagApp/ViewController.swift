d683272176cc227aac6faa45f6bbbf8fe6e1b735
Egmars Janis Timma <egmars.janis.timma@accenture.com>
2019-04-18 09:41:12 +0300

    Initial Commit

---
+++ guessTheFlagApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  guessTheFlagApp
+//
+//  Created by egmars.janis.timma on 18/04/2019.
+//  Copyright © 2019 egmars.janis.timma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
