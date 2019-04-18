c4936f7a1c07e7ded6b73bfbc7c10f1a2e8b2bdf
Egmars Janis Timma <egmars.janis.timma@accenture.com>
2019-04-18 10:45:53 +0300

    UIView with flag assets

--- guessTheFlagApp/ViewController.swift
+++ guessTheFlagApp/ViewController.swift
@@ -9,12 +9,41 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var button1: UIButton!
+    @IBOutlet var button2: UIButton!
+    @IBOutlet var button3: UIButton!
+    
+    var contries = [String]()
+    var score = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+//        contries.append("estonia")
+//        contries.append("france")
+//        contries.append("germany")
+//        contries.append("ireland")
+//        contries.append("italy")
+//        contries.append("monaco")
+        
+        contries += ["estonia", "france", "germany", "ireland", "italy", "monaco"]
+        
+        button1.layer.borderWidth = 1
+        button2.layer.borderWidth = 1
+        button3.layer.borderWidth = 1
+        
+        button1.layer.borderColor = UIColor.lightGray.cgColor
+        button2.layer.borderColor = UIColor.lightGray.cgColor
+        button3.layer.borderColor = UIColor.lightGray.cgColor
+        
+        askQuestion()
     }
 
-
+    func askQuestion (){
+        button1.setImage(UIImage (named: contries[0]), for: .normal)
+        button2.setImage(UIImage (named: contries[1]), for: .normal)
+        button3.setImage(UIImage (named: contries[2]), for: .normal)
+    }
+    
 }
 
